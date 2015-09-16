dec4f1838f29a17f1279ae6cc4266c5ebd3a7998
Brad Parbs <brad@bradparbs.com>
2015-09-16 15:27:06 -0500

    Rename formula

--- vvv-provision-flipper.rb
+++ vvv-provision-flipper.rb
@@ -1,4 +1,4 @@
-class Vvvprovisionflipper < Formula
+class VvvFlip < Formula
   homepage "https://github.com/bradp/vvv-provision-flipper"
   url "https://github.com/bradp/vvv-provision-flipper/archive/1.0.0.tar.gz"
   sha256 "c9699f5ab9340f811e6f0437d07708b492453f785ab6a82a8c80c2a5aadeaf21"
